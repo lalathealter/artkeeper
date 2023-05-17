c09c069a634778bf29facd5dc9a0e4446cca203b
lalathealter <musin.daniiil@yandex.ru>
2023-05-17 21:52:09 +0500

    added a separate package for authorization and hashing stuff; Implemented a JWT issuing procedure

---
+++ controllers/auth/auth.go
@@ -0,0 +1,71 @@
+package auth
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"log"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func BcryptString(input string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(input), bcrypt.DefaultCost)
+	if err != nil {
+		log.Panicln(err)
+	}
+	return string(hash)
+}
+
+const MAX_TOKEN_AGE = 60
+
+type HeaderJWT struct {
+	Alg string `json:"alg"`
+	Typ string `json:"typ"`
+}
+
+type PayloadJWT struct {
+	IAT int64 `json:"iat"`
+	EXP int64 `json:"exp"`
+	Name string `json:"name"`
+}
+
+func encodeJWTData(v any) string {
+	jsonBytes, err := json.Marshal(v)
+	if err != nil {
+		log.Panicln(err)
+	}
+	encodedString := base64.RawURLEncoding.EncodeToString(jsonBytes)
+	return encodedString
+}
+
+const JWT_ALG = "HS256"
+const JWT_TYP = "JWT"
+func IssueJWT(name string) string {
+	
+	header := HeaderJWT{JWT_ALG, JWT_TYP}
+
+	currTime := time.Now().Unix()
+	payload := PayloadJWT{
+		currTime,
+		currTime + MAX_TOKEN_AGE,
+		name,
+	}
+
+	headerStr := encodeJWTData(header)
+	payloadStr := encodeJWTData(payload)
+	bodyStr := headerStr + "." + payloadStr
+
+	signature := signJWT(bodyStr)
+	signedJWT := bodyStr + "." + signature
+	return signedJWT
+}
+
+func signJWT(body string) string {
+	hasher := hmac.New(sha256.New, []byte("SECRET"))
+	hasher.Write([]byte(body))
+	signature := base64.RawURLEncoding.EncodeToString(hasher.Sum(nil))
+	return signature
+}
--- controllers/registeruser.go
+++ controllers/registeruser.go
@@ -2,11 +2,10 @@ package controllers
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
 
+	"github.com/lalathealter/artkeeper/controllers/auth"
 	"github.com/lalathealter/artkeeper/models"
-	"golang.org/x/crypto/bcrypt"
 )
 
 var UserRegistrationHandler = factorAPIHandler(
@@ -16,17 +15,11 @@ var UserRegistrationHandler = factorAPIHandler(
 
 func readUserRegistrationRequest(r *http.Request) (models.Message, error) {
 	msg, err := parseJSONMessage(r, models.RegisterUserRequest{})
-	msg.Password.Update(bcryptString)
+	msg.Password.Update(auth.BcryptString)
 	return msg, err 
 }
 
-func bcryptString(input string) string {
-	hash, err := bcrypt.GenerateFromPassword([]byte(input), bcrypt.DefaultCost)
-	if err != nil {
-		log.Panicln(err)
-	}
-	return string(hash)
-}
+
 
 func respondUserRegistrationRequest(w http.ResponseWriter, dbr models.DBResult) {
 	execRes := dbr.(sql.Result)
