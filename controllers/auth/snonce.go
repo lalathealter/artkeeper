90538b4b339df5809c8564615715e0fd77eaafce
lalathealter <musin.daniiil@yandex.ru>
2023-05-23 22:11:15 +0500

    added a guard against request id collisions when generating snonces

--- controllers/auth/snonce.go
+++ controllers/auth/snonce.go
@@ -9,19 +9,29 @@ import (
 )
 
 const SNONCE_SIZE = 16
+const REQID_SIZE = 8
 const HeaderAuthReqId = "Authentication-Request-ID"
 const HeaderAuthServerNonce = "Authentication-Server-Nonce"
 var AuthRequests = make(map[string][SNONCE_SIZE]byte)
 
 func ServerNonceHandler(w http.ResponseWriter, r *http.Request) {
 	snonce := generateRandomByteArr(SNONCE_SIZE)
-	requestid := hex.EncodeToString(generateRandomByteArr(8))
+	requestid := generateUniqueRequestId(REQID_SIZE)
 	AuthRequests[requestid] = [SNONCE_SIZE]byte(snonce)
 	hexSnonce := hex.EncodeToString(snonce)
 
 	w.Header().Set(HeaderAuthReqId, requestid)
 	w.Header().Set(HeaderAuthServerNonce, hexSnonce)
 	w.WriteHeader(http.StatusOK)
+}
+
+func generateUniqueRequestId(size int) string {
+	idHexStr := hex.EncodeToString(generateRandomByteArr(size))
+	_, alreadyExists := AuthRequests[idHexStr]
+	if !alreadyExists {
+		return idHexStr
+	}
+	return generateUniqueRequestId(size)
 }
 
 func generateRandomByteArr(size int) []byte {
